f7c159bf48fe34ba551d4f06f98b2ce058e165e0
Zuruuh <zuruh.dev@proton.me>
2024-01-29 16:53:18 +0100

    Add og content and banner

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -14,6 +14,9 @@ export default defineConfig({
   head: [
     ['link', { rel: 'icon', href: '/favicon.svg' }],
     ['meta', { name: 'theme-color', content: '#61DAFB' }],
+    ['meta', { name: 'og:type', content: 'website' }],
+    ['meta', { name: 'og:locale', content: 'en' }],
+    ['meta', { name: 'og:image', content: 'https://date-picker.zuruh.dev/og.png' }],
     ['meta', { name: 'og:site_name', content: 'React Date Picker' }],
   ],
 
