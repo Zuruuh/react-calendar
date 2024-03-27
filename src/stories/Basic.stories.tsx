49579cb9cf210cabd4f0a816437f511f43b88ab0
Zuruuh <zuruh.dev@proton.me>
2024-03-27 01:13:22 +0100

    fix formatting

--- src/stories/Basic.stories.tsx
+++ src/stories/Basic.stories.tsx
@@ -1,6 +1,10 @@
 import styles from './Basic.stories.module.scss';
 import { useState, type FC } from 'react';
-import { DatePicker, type CalendarInnerProps, type DatePickerControls } from '../';
+import {
+  DatePicker,
+  type CalendarInnerProps,
+  type DatePickerControls,
+} from '../';
 import type { Story } from '@ladle/react';
 import { withStrictMode } from '../ladle/decorators/withStrictMode';
 import clsx from 'clsx';
